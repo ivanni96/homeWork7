e2e1c77b72361fff560d27769dbfd20cf8a1e488
ivan <ivanni95@gmail.com>
2022-10-09 20:16:02 +0400

    Решил 2 задачу

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,20 @@ public class Main {
             totalBankDeposit = totalBankDeposit + bankDeposit;
 
             System.out.println("Месяц " + a + " ,cумма накоплений равна " + totalBankDeposit);}
+
+        // Задача 2
+
+        int b = 0;
+        b++;
+        while (b<10){
+            b++;
+            System.out.println(b);}
+        for (int c = 10;c >= 0;c--){
+            System.out.println(c);}
+
+
+
+
+
     }
 }
